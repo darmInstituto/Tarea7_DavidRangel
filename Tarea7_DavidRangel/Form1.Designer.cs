6f47fdd0c9cd02748547a28c01253e04862d056a
Darm02 <david02rangel@gmail.com>
2023-05-11 11:11:46 -0400

    Agregar archivos de proyecto.

---
+++ Tarea7_DavidRangel/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace Tarea7_DavidRangel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtRut = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNota = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(82, 37);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Rut:";
+            // 
+            // txtRut
+            // 
+            this.txtRut.Location = new System.Drawing.Point(137, 34);
+            this.txtRut.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtRut.Name = "txtRut";
+            this.txtRut.Size = new System.Drawing.Size(148, 30);
+            this.txtRut.TabIndex = 1;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(137, 89);
+            this.txtNombre.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(148, 30);
+            this.txtNombre.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(42, 92);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nombre:";
+            // 
+            // txtNota
+            // 
+            this.txtNota.Location = new System.Drawing.Point(137, 147);
+            this.txtNota.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtNota.Name = "txtNota";
+            this.txtNota.Size = new System.Drawing.Size(148, 30);
+            this.txtNota.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(70, 150);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Nota:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(75, 210);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 46);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Guardar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(211, 210);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 46);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Mostrar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(430, 34);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(409, 366);
+            this.dataGridView1.TabIndex = 8;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(178, 305);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(148, 30);
+            this.textBox1.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(57, 308);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(113, 25);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Buscar Rut:";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(137, 343);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(109, 46);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Buscar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(953, 444);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtNota);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtRut);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtRut;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNota;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Tarea7_DavidRangel/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Tarea7_DavidRangel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string ruta = Application.StartupPath + @"\archivo\tabla.csv";
+        List<string> listRuts;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txtNombre.Text != String.Empty && txtRut.Text != String.Empty && txtNota.Text != String.Empty)
+            {
+                if (!listRuts.Contains(txtRut.Text))
+                {
+                    StreamWriter sw = File.AppendText(ruta);
+                    string registro = String.Format("{0};{1};{2}", txtRut.Text, txtNombre.Text, txtNota.Text);
+                    sw.WriteLine(registro);
+                    sw.Close();
+                    listRuts.Add(txtRut.Text);
+                }
+                else
+                {
+                    if (MessageBox.Show("Rut ya fue ingresado. ¿Desea modificar?", "Advertencia", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                    {
+                        StreamReader sr = new StreamReader(ruta);          
+                        string lectura;
+                        lectura = sr.ReadLine();
+                        int i = 0;
+                        int posicion = 0;
+                        string[,] tabla = new string[listRuts.Count+1, 3];
+                        while (lectura != null)
+                        {
+                            string[] registro = lectura.Split(';');
+
+                            for (int col = 0; col < 3; col++)
+                            {
+                                tabla[i, col] = registro[col];
+                            }
+
+                            if (registro[0] == txtRut.Text)
+                            {
+                                posicion = i;
+                            }
+                            i++;
+                            lectura = sr.ReadLine();
+                        }
+                        sr.Close();
+
+                        tabla[posicion, 1] = txtNombre.Text;
+                        tabla[posicion, 2] = txtNota.Text;
+
+                        string texto = "";
+                        for (int fil = 0; fil < tabla.GetLength(0); fil++)
+                        {
+                            texto += String.Format("{0};{1};{2}\n", tabla[fil, 0], tabla[fil, 1], tabla[fil, 2]);
+                        }
+                        File.WriteAllText(ruta, texto);
+                    }
+                }
+                txtRut.Clear();
+                txtNombre.Clear();
+                txtNota.Clear();
+                txtRut.Focus();
+            }
+            else
+            {
+                MessageBox.Show("Campo vacio");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listRuts = new List<string>();
+            bool existe = File.Exists(ruta);
+
+            if (existe)
+            {
+                StreamReader sr = new StreamReader(ruta);
+                string lectura;
+                lectura = sr.ReadLine();
+                if (lectura != "rut;nombre;nota")
+                {
+                    string headers = "rut;nombre;nota\n";
+                    sr.Close();
+                    File.WriteAllText(ruta, headers);
+                }
+                else
+                {
+                    lectura = sr.ReadLine();
+                    while (lectura != null)
+                    {
+                        string[] registro = lectura.Split(';');
+                        listRuts.Add(registro[0]);
+                        lectura = sr.ReadLine();
+                    }
+                    sr.Close();
+                }
+            }
+            else
+            {
+                string headers = "rut;nombre;nota\n";
+                File.WriteAllText(ruta, headers);
+            }
+                     
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            StreamReader sr = new StreamReader(ruta);
+            String lectura;
+            lectura = sr.ReadLine();
+            dataGridView1.ColumnCount = 3;
+            while (lectura != null)
+            {
+                string[] registro = lectura.Split(';');
+
+                if (registro[0] == "rut")
+                {
+                    dataGridView1.Columns[0].HeaderText = registro[0];
+                    dataGridView1.Columns[1].HeaderText = registro[1];
+                    dataGridView1.Columns[2].HeaderText = registro[2];
+                }
+                else
+                {
+                    dataGridView1.Rows.Add(registro);
+                }
+                lectura = sr.ReadLine();
+            }
+            sr.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            StreamReader sr = new StreamReader(ruta);
+            string lectura;
+            lectura = sr.ReadLine();
+            
+            string nombreFinded="", rutFinded="", notaFinded="";
+            while (lectura != null)
+            {
+                string[] registro = lectura.Split(';');
+
+                if (registro[0] == textBox1.Text)
+                {                   
+                    rutFinded = registro[0];
+                    nombreFinded = registro[1];
+                    notaFinded = registro[2];
+                }             
+                lectura = sr.ReadLine();
+            }
+            sr.Close();
+
+            txtRut.Text = rutFinded;
+            txtNombre.Text = nombreFinded;
+            txtNota.Text = notaFinded;
+
+
+        }
+    }
+}
